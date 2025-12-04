5b5a84cdcc40093ce3f1465d369a1887a95ad248
jreisman2029 <109403545+PrismaticDepths@users.noreply.github.com>
2025-12-03 23:44:29 -0800

    finished day 3 puzzle (yay)

--- three/puzzle_2.py
+++ three/puzzle_2.py
@@ -7,7 +7,7 @@ with open("input.txt","r") as input_file:
 	voltages:str = read_puzzle_input(input_file.read())
 
 current:int = 0
-times = 2
+times = 12
 for i in voltages:
 	highest_numbers:int = [0 for n in range(times)]
 	index:int = -1
@@ -16,14 +16,12 @@ for i in voltages:
 		cur:int = index
 		for j in i[index+1:]:
 			cur += 1
-			if int(j) > highest_numbers[n] and cur < len(i)-((times-n)-1):
+			if int(j) > highest_numbers[n] and cur < len(i)-(times-(n+1)):
 				highest_numbers[n] = int(j)
 				index = cur
-			cur:int = index
 	for j in i[index+1:]:
 		if max(int(j), highest_numbers[times-1]) == int(j):
 			highest_numbers[times-1] = int(j)
 	current += int("".join(map(lambda f: str(f),highest_numbers)))
+	
 print(f"Answer: {current}")
-
-#178334067355214: too high
